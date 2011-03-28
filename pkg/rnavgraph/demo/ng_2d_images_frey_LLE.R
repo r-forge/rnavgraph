d9b2368be7fde41327684d41ce7add563ba202f6
awaddell (Adrian Waddell) <adrian@waddell.ch>
2011-03-28 22:20:14 +0000

    Adding demo using Local Linear Embedding from the RDRToolbox.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rnavgraph@27 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/rnavgraph/demo/ng_2d_images_frey_LLE.R
@@ -0,0 +1,28 @@
+require(RnavGraph) || stop("RnavGraph library not available")
+require(RnavGraphImgData) || stop('You need the RnavGraphImgData package installed!')
+require(RDRToolbox)|| stop('You need the RDRToolbox package installed!')
+
+local({
+			data(frey)
+			
+			## LLE
+			d_low <- LLE(t(frey),dim=5,k=12)
+			
+			## Images
+			## sample a few
+			sel <- sample(1:dim(frey)[2],600, replace = FALSE)
+			ng.frey <- ng_image_array_gray('Brendan_Frey',frey[,sel],28,20, img_in_row = FALSE, rotate = 90)
+			ng.frey
+			
+			
+			
+			ng.lle.frey <- ng_data(name = "ISO_frey",
+					data = data.frame(d_low[sel,]),
+					shortnames = mapply(function(x){paste("i",x,sep="")},1:5))
+			
+			nav <- navGraph(ng.lle.frey,c("Clumpy","Outlying","Skewed","Monotonic", "Striated"),
+					images=ng.frey)
+			
+		})
+
+cat(paste("\n\nThe source code of this demo file is located at:\n",system.file("demo", "ng_2d_images_frey.R", package="RnavGraph"),"\n\n\n"))
